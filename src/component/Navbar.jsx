bbc567548b976442798e844604488415d2aa7fb6
mayur-chace <mayur.kamthe@lockrfinance.com>
2023-02-23 13:04:19 +0530

    scroll issue fixed with z-index

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 function Navbar() {
   const navigate = useNavigate();
   return (
-    <div className='absolute w-screen text-[18px] font-medium font-rubik px-[25px] h-[65px] flex justify-between items-center bg-white'>
+    <div className='absolute w-screen text-[18px] font-medium font-rubik px-[25px] h-[65px] flex justify-between items-center bg-white z-[9999999999]'>
       <div className='cursor-pointer' onClick={() => { navigate('/blog') }}><WolibaImg /></div>
       <div className='flex gap-[15px] items-center'>
         <div className='mr-[10px]'>
